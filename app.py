96ef9e53eaa6921d88806aa60190eac27f0dbf02
omarlopez2001 <omarlopez9445@gmail.com>
2021-03-18 16:01:26 -0400

    templates
    implemented templates

--- app.py
+++ app.py
@@ -1,13 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Omar Lopez',
+        'title': 'First Post',
+        'content': 'Hi, my name is Omar Lopez',
+        'date_posted': 'March 18, 2021'
+    },
+    {
+        'author': 'Lebron James',
+        'title': 'Second Post',
+        'content': 'Hi, my name is Lebron James',
+        'date_posted': 'March 19, 2021'
+    }
+]
 
 @app.route('/')
 def home():
-    return '<h1>Home Page</h1>'
+    return render_template('home.html', posts=posts)
 
 @app.route('/about')
 def about():
-    return '<h1>About Page</h1>'
+    return render_template('about.html', title='About')
 
 if __name__ == '__main__':
     app.run(debug=True)
