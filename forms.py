b595c3dd268ece9a0ea447f1f453e1b73e03d633
omarlopez2001 <omarlopez9445@gmail.com>
2021-03-19 10:40:18 -0400

    Erased email for signup
    Users will login with username. Email is not necessary.

--- forms.py
+++ forms.py
@@ -1,12 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, EqualTo
 
 class SignUpForm(FlaskForm):
     username = StringField('User Name',
         validators=[DataRequired(), Length(min=3, max=50)])
-    email = StringField('Email',
-        validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', 
         validators=[DataRequired(), EqualTo('password')])
