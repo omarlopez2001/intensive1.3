89fe66b813793b38d443b81476d348ac8cf8ebc7
omarlopez2001 <omarlopez9445@gmail.com>
2021-03-18 21:18:18 -0400

    Added login route and template

--- app.py
+++ app.py
@@ -35,9 +35,15 @@ def signup():
         return redirect(url_for('home'))
     return render_template('signup.html', title='Sign Up', form=form)
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.username.data == 'omarlopez' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
     return render_template('login.html', title='Login', form=form)
 
 if __name__ == '__main__':
