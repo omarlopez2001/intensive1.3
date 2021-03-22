60900205c440b9ecac5d85ff5034ff2ea877a302
omarlopez2001 <omarlopez9445@gmail.com>
2021-03-22 12:24:19 -0400

    implemented authentication
    used flask-bcrypt and flask-login extensions. Also created logout and account routes when users login.

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, EqualTo
+from wtforms.validators import DataRequired, Length, EqualTo, ValidationError
+from app.models import User
 
 class SignUpForm(FlaskForm):
     username = StringField('User Name',
@@ -9,6 +10,11 @@ class SignUpForm(FlaskForm):
     confirm_password = PasswordField('Confirm Password', 
         validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That username is already taken. Please choose a different one.')
 
 class LoginForm(FlaskForm):
     username = StringField('User Name',
