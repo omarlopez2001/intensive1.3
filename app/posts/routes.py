1dab55c9609efdb88f7678345e9859352a20b173
omarlopez2001 <omarlopez9445@gmail.com>
2021-06-17 11:01:42 -0400

    Fixed PostForm NameError

--- app/posts/routes.py
+++ app/posts/routes.py
@@ -1,5 +1,8 @@
 from flask import render_template, url_for, flash, redirect, request, abort, Blueprint
 from flask_login import current_user, login_required
+from app import db
+from app.models import Post
+from app.posts.forms import PostForm
 
 posts = Blueprint('posts', __name__)
 
